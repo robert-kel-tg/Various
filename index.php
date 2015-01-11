f08e7da2e76b0367cb23c232c3f6b812aaff3f43
Robert Kel <kel.robert@gmail.com>
2015-01-11 18:08:13 +0200

    Init

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
+namespace test1;
 
-$log_details = new LogDetails(
+use test1\Src\LogDetails;
+use test1\Src\User;
+use test1\Src\Log;
+
+require 'vendor/autoload.php';
+
+$log_details = new LogDetails();
+$log_get_details = $log_details->getDetails(
     new User('Jonas'),
     new Log('metinis')
 );
+
+echo '<pre>';die(print_r($log_get_details));echo '</pre>';
